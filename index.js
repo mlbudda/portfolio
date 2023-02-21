9e701e340afdedc8e41bf33d1080080d108e67c6
Ignas Markauskas <ignasmark5@gmail.com>
2023-02-21 10:37:41 +0200

    added testing repoContent

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dir = window.location.href;
-
+const repoContent = await github.rest.repos.getContent({
+  owner: "repo-owner",
+  repo: "repo-name",
+});
 const list = document.querySelector("#solution-list");
 
 console.log(dir);
+console.log(repoContent);
