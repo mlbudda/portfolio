8ac06091581cf4efd5f45b3c645d72ec14aea074
Ignas Markauskas <ignasmark5@gmail.com>
2023-02-22 10:22:42 +0200

    simple fetching all projects from github portfolio

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const dir = window.location.href;
-const repoContent = await github.rest.repos.getContent({
-  owner: "repo-owner",
-  repo: "repo-name",
-});
 const list = document.querySelector("#solution-list");
 
-console.log(dir);
-console.log(repoContent);
+const baseUrl = "https://api.github.com";
+const endPoint = "/repos/mlbudda/portfolio/contents/";
+
+fetch(baseUrl + endPoint)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const allDir = data.filter((item) => item.type === "dir");
+    allDir.forEach((element) => {
+      list.insertAdjacentHTML(
+        "beforeend",
+        `
+      <li>
+        <h2>${element.name}</h2>
+        <a href="https://github.com/mlbudda/portfolio/tree/master/${element.path}" target="_blank" rel="noopener noreferrer">Repo</a>
+        <a href="https://mlbudda.github.io/portfolio/${element.path}" target="_blank" rel="noopener noreferrer">Live</a>
+      </li>`
+      );
+    });
+  });
