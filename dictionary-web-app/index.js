37a6fd06ed606351babe16bd8d20e0111e5578d7
Ignas Markauskas <ignasmark5@gmail.com>
2023-03-25 12:56:20 +0200

    added fetchwrapper class with loaders

--- dictionary-web-app/index.js
+++ dictionary-web-app/index.js
@@ -2,8 +2,16 @@ import { changeFont, changeTheme, handleUserInput } from "./helpers.js";
 
 export const baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
+const fonts = [
+  {
+    "Sans Serif": "font--sans-serif",
+  },
+  { Sans: "font--serif" },
+  { Mono: "font--mono" },
+];
+
 const render = () => {
-  changeFont();
+  changeFont(fonts);
   changeTheme();
   handleUserInput(baseUrl);
 };
