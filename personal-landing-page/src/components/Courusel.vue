7dae0b704cf8e26af1f6d38fe606b5c2452b6d07
Ignas Markauskas <ignasmark5@gmail.com>
2023-05-01 17:24:53 +0300

    first working done

---
+++ personal-landing-page/src/components/Courusel.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-expand-x-transition>
+        <v-carousel height="auto" v-show="expand" hide-delimiters progress="primary" touch>
+            <v-carousel-item v-for="(slide, i) in  slides " :key="i">
+                <v-sheet class="d-flex fill-height align-center">
+                    <v-container class="d-flex fill-height justify-center">
+                        <v-row align="center" class="mx-12">
+                            <v-col align="center">
+                                <v-sheet elevation="1">
+                                    <v-img aspect-ratio="1/1" max-height="600" min-width="200" :src="slide.image">
+                                    </v-img>
+                                </v-sheet>
+                            </v-col>
+                            <v-col>
+                                <!-- <v-sheet>{{ slide.desc }}</v-sheet> -->
+                                <v-card elevation="0" max-width="400">
+                                    <v-card-title>
+                                        {{ slide.name }}
+                                    </v-card-title>
+                                    <v-card-text>
+                                        {{ slide.desc }}
+                                    </v-card-text>
+                                    <v-divider></v-divider>
+                                    <v-card-actions class="">
+                                        <v-spacer></v-spacer>
+                                        <v-chip v-for="item in slide.stack" :index="item.name"
+                                            :prepend-icon="`mdi-${item.icon ? item.icon : 'lightbulb-outline'}`"
+                                            variant="elevated">
+                                            {{ item.name }}
+                                        </v-chip>
+                                        <v-spacer></v-spacer>
+                                    </v-card-actions>
+                                    <v-card-actions>
+                                        <v-btn :href="slide.liveUrl" :disabled="slide.liveUrl !== '' ? false : true"
+                                            target="_blank">Live site</v-btn>
+                                        <v-spacer></v-spacer>
+                                        <v-btn :href="slide.repo" :disabled="slide.repo !== '' ? false : true"
+                                            target="_blank">Repository</v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-sheet>
+            </v-carousel-item>
+        </v-carousel>
+    </v-expand-x-transition>
+</template>
+  
+<script setup>
+import { ref } from "vue"
+defineProps(['expand'])
+
+const slides = [
+    {
+        id: 0,
+        name: "Boutique e-shop - Yuga",
+        image: "yuga-export.jpg",
+        desc: `Full-stack app with custom checkout flow using Bulma UI and Vue. It includes client-side payment integration and a simple backend framework for operations.`,
+        liveUrl: "https://yuga.lt",
+        repo: '',
+        stack: [{ name: "Bulma", icon: "bulma" }, { name: "VueJs", icon: "vuejs" }, { name: "NuxtJs", icon: "nuxt" }, { name: "Stripe", icon: "" }]
+    },
+    {
+        id: 1,
+        name: `Dev jobs ad app`,
+        image: "dev-jobs.jpg",
+        desc: `FrontendMentor challenge - production-ready front-end application built with
+              Vue.js and Nuxt.js, bundled together using
+              Vite.`,
+        liveUrl: "https://chimerical-dieffenbachia-00afd0.netlify.app/",
+        repo: 'https://github.com/mlbudda/portfolio/tree/master/devjobs-web-app/frontend',
+        stack: [{ name: "HTML", icon: "language-html5" }, { name: "CSS", icon: "language-css3" }, { name: "NuxtJs", icon: "nuxt" }, { name: "VueJs", icon: "vuejs" }]
+    },
+    {
+        id: 2,
+        name: `Dictionary app`,
+        image: "dictionary-screenshot.jpg",
+        desc: `FrontendMentor challenge - I’ve used pure HTML, CSS and
+              JS to call API and display results. It has
+              custom error checking and dark theme which match
+              user preferred theme.`,
+        liveUrl: "https://mlbudda.github.io/portfolio/dictionary-web-app",
+        repo: 'https://github.com/mlbudda/portfolio/tree/master/dictionary-web-app',
+        stack: [{ name: "HTML", icon: "language-html5" }, { name: "CSS", icon: "language-css3" }, { name: "JavaScript", icon: "language-javascript" }, { name: "API", icon: "api" }]
+    }
+]
+const show = ref(false)
+
+</script>
---
+++ personal-landing-page/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <Hero @show-work="expand = true" />
+  <Courusel :expand="expand" />
+  <v-footer absolute v-show="expand" class="mt-4" border>
+    <v-container>
+      <v-row>
+        <v-col class="d-flex align-center justify-center">2023 - <v-btn class="text-lowercase" append-icon="mdi-github"
+            variant="plain">
+            mlbudda
+          </v-btn></v-col>
+      </v-row>
+
+    </v-container>
+  </v-footer>
+</template>
+
+<script setup>
+import Hero from "../components/Hero.vue"
+import Courusel from "../components/Courusel.vue"
+import { ref } from "vue"
+
+const expand = ref(false)
+const isSkillActive = ref(false)
+
+
+</script>
