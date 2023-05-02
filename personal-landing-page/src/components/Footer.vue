24343928d57eff3efccfc4b1d599346350d50f35
Ignas Markauskas <ignasmark5@gmail.com>
2023-05-02 17:10:38 +0300

    added footer

---
+++ personal-landing-page/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-footer absolute v-show="expand" class="mt-4">
+        <v-container>
+            <v-row>
+                <v-col class="d-flex align-center justify-center">2023 - <v-btn class="text-lowercase"
+                        append-icon="mdi-github" variant="plain">
+                        mlbudda
+                    </v-btn></v-col>
+            </v-row>
+        </v-container>
+    </v-footer>
+</template>
+
+<script setup>
+defineProps(['expand'])
+
+
+</script>
