1a3293fdd884b188a85ae5e4ee58bcbad4239f8e
Ignas Markauskas <ignasmark5@gmail.com>
2023-05-03 16:03:58 +0300

    Fixed carousel resizing bug & improved respo design

--- personal-landing-page/src/components/HireMe.vue
+++ personal-landing-page/src/components/HireMe.vue
@@ -3,16 +3,16 @@
         <v-container class="py-12">
             <v-row justify="start">
                 <v-col>
-                    <h2 class="banner2-title font-weight-light text-grey-darken-2">
-                        <v-icon icon="mdi-greater-than" size="x-small"></v-icon>Are you looking who
+                    <h2 class="banner2-title font-weight-light text-grey-darken-2 text-sm-h6 text-body-1"> > Are you looking
+                        who
                         can build
                         scalable, performant, and visually appealing web
                         applications?
                     </h2>
-                    <h2 class="banner2-title font-weight-light text-grey-darken-2">
-                        <v-icon icon="mdi-greater-than" size="x-small"></v-icon>
-                        <v-btn variant="tonal" href="mailto:ignasmark5@gmail.com"
-                            append-icon="mdi-email-arrow-left-outline">Let's talk
+                    <h2 class="banner2-title font-weight-light text-grey-darken-2 text-sm-h6 text-body-1"> > <v-btn
+                            variant="tonal" href="mailto:ignasmark5@gmail.com" append-icon="mdi-email-arrow-left-outline"
+                            size="small">Let's
+                            talk
                             <template v-slot:append>
                                 <v-icon color="deep-purple-accent-3"></v-icon>
                             </template>
